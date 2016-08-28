6c9b52b628ed2c636ce0f98459d3384b8e3a3243
t68000 <t68030@gmail.com>
2016-08-28 22:41:54 +0900

    第二弾

---
+++ db_test2.php
@@ -0,0 +1,14 @@
+<?php
+//new-featureの変更（１）
+//new-featureの変更（２）
+$link = mysqli_connect('127.0.0.1', 'test', 'Test1234!');
+if (!$link) {
+die('接続失敗！
+'.mysqli_connect_error());
+} else {
+echo '接続成功！';
+mysqli_set_charset($link,"utf8");
+echo '文字コードは'.mysqli_character_set_name($link).'です。';
+mysqli_close($link);
+}
+?>
